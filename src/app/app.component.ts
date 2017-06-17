e3601314ad2eb235e1d75e638b273e539dc987b4
Maciej Sawicki <maciej.sawicki@acaisoft.com>
2017-06-17 14:42:58 +0200

    Init Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild("expandableTable") expandableTable: any;
+
+  cities = [{
+    name: "New York"
+  }, {
+    name: "Chicago"
+  }, {
+    name: "Boston"
+  }, {
+    name: "Paris"
+  }, {
+    name: "Barcelona"
+  }, {
+    name: "Madrid"
+  }];
+
+  constructor() {
+
+  }
+
+  public toggle(row) {
+    this.expandableTable.rowDetail.toggleExpandRow(row);
+  }
 }
